e46e46d78c9e6b1e894094a9ef2ab657e8df3bfd
= <=>
2026-05-28 08:06:40 -0400

    Moving computers, so bring repositiory up to date. Added base for everything, very simple html and css. As well as the basis for Js. Mostly testing to make sure verything works.

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-var test;
+var money = 0;
+
+document.addEventListener('DOMContentLoaded', (Event) => {
+    console.log("Everything Loaded :D");
+});
+
+document.getElementById("promptButton").addEventListener('click', (MouseEvent) => {
+    changeMoney(1);
+});
+
+
+
+
+function changeMoney(amount) {
+    if (money + amount > 0) {
+        money = money + amount;
+    } else {
+        console.log("There was an attempt to bring money below 0:\nCurrent money: " + money + "\nAmount being removed: " + amount);
+        return;
+    }
+    console.clear();
+    console.log("Current money: " + money)
+}
+
+
